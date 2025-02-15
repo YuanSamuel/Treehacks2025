b95fa9c737f984a1e5dfff2e09f864e03789a3f8
YuanSamuel <syuan@utexas.edu>
2025-02-14 21:29:45 -0800

    init repo

---
+++ ios/antenna/antennaApp.swift
@@ -0,0 +1,17 @@
+//
+//  antennaApp.swift
+//  antenna
+//
+//  Created by Samuel Yuan on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct antennaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
